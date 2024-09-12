202e221ff0415be5c6ca5067cfd9f5be6eef65ef
zuhairm2001 <zuhairm2001@hotmail.com>
2024-09-12 16:43:34 +1000

    finished error handling exercises

--- exercises/13_error_handling/errors5.rs
+++ exercises/13_error_handling/errors5.rs
@@ -12,7 +12,7 @@
 // can be returned in a `Result`.
 
 use std::error::Error;
-use std::fmt;
+use std::{fmt, i64};
 
 #[derive(PartialEq, Debug)]
 enum CreationError {
@@ -48,7 +48,7 @@ impl PositiveNonzeroInteger {
 
 // TODO: Add the correct return type `Result<(), Box<dyn ???>>`. What can we
 // use to describe both errors? Is there a trait which both errors implement?
-fn main() {
+fn main() -> Result<(),Box<dyn Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
